a0dc7ca6e6963c691ec8454f0bf6452f8b4fa1f4
ebm539 <ebm539@protonmail.com>
2019-03-25 13:32:45 +1300

    Fix back button, add delete GUI buttons, add save/load

--- spirolateral.py
+++ spirolateral.py
@@ -2,6 +2,8 @@
 from tkinter import Tk, Frame, Button, Label, Entry
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 from dataclasses import dataclass
+from ast import literal_eval
+import pickle
 
 
 @dataclass
@@ -21,7 +23,6 @@ class SpirolateralGUI(Frame):
 
         self.main_menu = Frame(master, width=300, height=200)
         self.main_menu.grid_propagate(0)
-        self.main_menu.grid()
 
         self.add_spirolateral = Frame(master, width=300, height=200)
         self.add_spirolateral.grid_propagate(0)
@@ -43,6 +44,7 @@ class SpirolateralGUI(Frame):
         self.delete_spirolateral.grid_forget()
         self.save_spirolateral.grid_forget()
         self.load_spirolateral.grid_forget()
+        self.main_menu.grid_propagate(0)
         self.add = Button(self.main_menu, text="Add a spirolateral",
                           command=self.add_spirolateral_grid)
         self.add.grid(row=0, column=0)
@@ -60,6 +62,7 @@ class SpirolateralGUI(Frame):
                            command=self.master.quit)
         self.quit.grid(row=4, column=0)
         self.vcmd = (self.master.register(self.validate), '%P')
+        self.main_menu.grid()
 
     def add_spirolateral_grid(self):
         self.main_menu.grid_forget()
@@ -97,15 +100,42 @@ class SpirolateralGUI(Frame):
         self.add_spirolateral.grid()
 
     def delete_spirolateral_grid(self):
+        self.spirolateral_delete_no = Label(
+                self.delete_spirolateral, text="Spirolateral no. to delete: ")
+        self.spirolateral_delete_no.grid(row=2, column=0)
+        self.spirolateral_delete_no_entry = Entry(
+            self.delete_spirolateral, validate='key', validatecommand=self.vcmd)
+        self.spirolateral_delete_no_entry.grid(row=2, column=1)
         self.main_menu.grid_forget()
+        self.quit = Button(self.delete_spirolateral, text="Quit",
+                           command=self.master.quit)
+        self.quit.grid(row=4, column=0)
+
+        self.back = Button(self.delete_spirolateral, text="Back",
+                           command=self.main_menu_grid)
+        self.back.grid(row=5, column=0)
+
+        self.delete = Button(self.delete_spirolateral, text="Delete spirolateral",
+                            command=self.delete_spirolateral)
+        self.delete.grid(row=6, column=0)
+        self.delete_spirolateral.grid()
 
     def save_spirolateral_grid(self):
         # self.main_menu.grid_forget()
         filename = asksaveasfilename()
+        pickle_out = open(filename, 'wb')
+        pickle.dump(self.spirolaterals, pickle_out)
+        pickle_out.close()
+            
 
     def load_spirolateral_grid(self):
         # self.main_menu.grid_forget()
         filename = askopenfilename()
+        try:
+            pickle_in = open(filename, 'rb')
+            self.spirolaterals = pickle.load(pickle_in)
+        except ValueError:
+            print("This error should be in the GUI. The data cannot be read.")
 
     def validate(self, user_input):
         try:
@@ -116,9 +146,10 @@ class SpirolateralGUI(Frame):
             return False
 
     def new_spirolateral(self):
-        spirolateral = Spirolateral(self.spirolateral_name_entry.get(
-        ), self.spirolateral_segments_entry.get(), self.spirolateral_angle_entry.get())
-        self.spirolaterals.append(spirolateral)
+        self.spirolaterals.append(Spirolateral(self.spirolateral_name_entry.get(), self.spirolateral_segments_entry.get(), self.spirolateral_angle_entry.get()))
+
+    def delete_spirolateral(self):
+        del self.spirolaterals[self.spirolateral_delete_no_entry.get()]
 
 
 if __name__ == '__main__':
