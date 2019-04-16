028d615ad97575facf120b015f8e45a5471a90ea
ebm539 <ebm539@protonmail.com>
2019-04-17 07:37:21 +1200

    Finally fix input validation (UI is still broken)

--- spirolateral.py
+++ spirolateral.py
@@ -52,7 +52,7 @@ class SpirolateralGUI(Frame):
         master.title("Spirolateral")
 
         self.spirolaterals = []
-        self.index = len(self.spirolaterals)
+        self.index = 0
 
         self.header_row = Frame(master)
         self.main_menu = Frame(master)
@@ -65,14 +65,18 @@ class SpirolateralGUI(Frame):
         self.add = Button(self.header_row, text="Add a spirolateral",
                           command=self.add_spirolateral_grid)
         self.add.grid(row=0, column=0)
+        self.show_spirolaterals = Button(
+                self.header_row, text="Show spirolaterals",
+                command=self.main_menu_grid)
         self.delete = Button(
             self.header_row, text="Delete spirolateral",
             command=self.delete_spirolateral)
         self.delete.grid(row=0, column=1)
-        if len(self.spirolaterals):
-            self.delete.configure(state=NORMAL)
-        else:
-            self.delete.configure(state=DISABLED)
+        self.delete.configure(state=DISABLED)
+        #if len(self.spirolaterals):
+        #    self.delete.configure(state=NORMAL)
+        #else:
+        #    self.delete.configure(state=DISABLED)
         self.save = Button(self.header_row, text="Save spirolaterals",
                            command=self.save_spirolateral_grid)
         self.save.grid(row=0, column=2)
@@ -86,52 +90,66 @@ class SpirolateralGUI(Frame):
 
     def main_menu_grid(self):
         # forget previous grids, then grid main menu buttons as necessary
+        print(self.index)
+        self.header_row.grid_forget()
+        self.header_row_grid()
         self.add_spirolateral.grid_forget()
         self.name = Label(self.main_menu, text="Name")
-        self.name.grid(row=0, column=0)
         self.name_display = Label(self.main_menu, text="")
-        self.name_display.grid(row=0, column=1)
         self.times_table = Label(self.main_menu,
                                  text="Times table")
-        self.times_table.grid(row=1, column=0)
         self.times_table_display = Label(self.main_menu,
                                          text="")
-        self.times_table_display.grid(row=1, column=1)
         self.angle = Label(self.main_menu, text="Angle")
-        self.angle.grid(row=2, column=0)
         self.angle_display = Label(self.main_menu, text="")
-        self.update_display()
-        self.angle_display.grid(row=2, column=1)
 
         self.previous = Button(self.main_menu, text="Previous",
                                command=self.display_previous)
-        self.previous.grid(row=3, column=0)
 
         self.delete = Button(self.main_menu, text="Delete",
                              command=self.delete_spirolateral)
-        self.delete.grid(row=3, column=1)
 
         self.next = Button(self.main_menu, text="Next",
                            command=self.display_next)
-        self.next.grid(row=3, column=2)
+        self.no_spirolaterals = Label(
+                self.main_menu, text="There are no spirolaterals")
+
+        if len(self.spirolaterals):
+            self.name.grid(row=0, column=0)
+            self.name_display.grid(row=0, column=1)
+            self.times_table.grid(row=1, column=0)
+            self.angle.grid(row=2, column=0)
+            self.angle_display.grid(row=2, column=1)
+            self.update_display()
+            self.previous.grid(row=3, column=0)
+            self.delete.grid(row=3, column=1)
+            self.next.grid(row=3, column=2)
+        else:
+            self.no_spirolaterals.grid(row=0, column=0)
 
         self.main_menu.grid(row=1, column=0, sticky='nesw')
 
     def add_spirolateral_grid(self):
         # forget previous grid, then grid labels and text boxes as necessary
-        self.vcmd = (self.master.register(self.validate), '%d', '%P')
+        self.vcmd = (self.master.register(self.validate), '%d', '%P', '%S')
         self.main_menu.grid_forget()
+        self.add.grid_forget()
+        self.show_spirolaterals.grid(row=0, column=0)
         self.spirolateral_name = Label(self.add_spirolateral, text="Name: ")
         self.spirolateral_name.grid(row=0, column=0)
         self.spirolateral_name_entry = Entry(self.add_spirolateral)
         self.spirolateral_name_entry.grid(row=0, column=1)
+        self.spirolateral_name_error = Label(
+                self.add_spirolateral, text="No name entered")
 
         self.spirolateral_times_table = Label(
-            self.add_spirolateral, text="Times_table: ")
+            self.add_spirolateral, text="Times table: ")
         self.spirolateral_times_table.grid(row=1, column=0)
         self.spirolateral_times_table_entry = Entry(
             self.add_spirolateral, validate='key', validatecommand=self.vcmd)
         self.spirolateral_times_table_entry.grid(row=1, column=1)
+        self.spirolateral_times_table_error = Label(
+                self.add_spirolateral, text="No times table entered")
 
         self.spirolateral_angle = Label(
             self.add_spirolateral, text="Angle of spirolateral: ")
@@ -139,6 +157,8 @@ class SpirolateralGUI(Frame):
         self.spirolateral_angle_entry = Entry(
             self.add_spirolateral, validate='key', validatecommand=self.vcmd)
         self.spirolateral_angle_entry.grid(row=2, column=1)
+        self.spirolateral_angle_error = Label(
+                self.add_spirolateral, text="No angle entered")
 
         self.enter = Button(self.add_spirolateral, text="Enter data",
                             command=self.new_spirolateral)
@@ -173,12 +193,13 @@ class SpirolateralGUI(Frame):
             # user cancelled file selection
             pass
 
-    def validate(self, action, user_input):
+    def validate(self, action, value_if_allowed, value):
         # validate input - allow if delete or entering int
+        if action == "0":
+            return True
         try:
-            if action:
-                return True
-            if int(user_input):
+            if value in '0123456789':
+                int(value_if_allowed)
                 return True
             raise ValueError
         except ValueError:
@@ -187,6 +208,29 @@ class SpirolateralGUI(Frame):
     def new_spirolateral(self):
         # make instance of spirolateral with data from text boxes,
         # then append to list and delete text box data
+
+        name = self.spirolateral_name_entry.get()
+        times_table = self.spirolateral_times_table_entry.get()
+        angle = self.spirolateral_angle_entry.get()
+
+        error_raised = False
+        # forget previous error messages
+        self.spirolateral_name_error.grid_forget()
+        self.spirolateral_times_table_error.grid_forget()
+        self.spirolateral_angle_error.grid_forget()
+
+        if name == "":
+            self.spirolateral_name_error.grid(row=0, column=2)
+            error_raised = True
+        if times_table == "":
+            self.spirolateral_times_table_error.grid(row=1, column=2)
+            error_raised = True
+        if angle == "":
+            self.spirolateral_angle_error.grid(row=2, column=2)
+            error_raised = True
+        if error_raised:
+            return False
+
         self.spirolaterals.append(Spirolateral(
             self.spirolateral_name_entry.get(),
             self.spirolateral_times_table_entry.get(),
@@ -197,8 +241,10 @@ class SpirolateralGUI(Frame):
         self.main_menu_grid()
 
     def delete_spirolateral(self):
-        # delete spirolateral from list
-        pass
+        # TODO: fix
+        self.spirolaterals.pop[self.index]
+        self.index -= 1
+        self.update_display()
 
     def display_previous(self):
         self.index -= 1
@@ -206,6 +252,7 @@ class SpirolateralGUI(Frame):
             self.previous.configure(state=DISABLED)
         else:
             self.previous.configure(state=NORMAL)
+        self.update_display()
 
     def display_next(self):
         self.index += 1
@@ -213,8 +260,11 @@ class SpirolateralGUI(Frame):
             self.next.configure(state=DISABLED)
         else:
             self.next.configure(state=NORMAL)
+        self.update_display()
 
     def update_display(self):
+        print(self.index)
+        print(len(self.spirolaterals))
         if self.index:
             self.name_display.configure(
                     text=self.spirolaterals[self.index].name)
